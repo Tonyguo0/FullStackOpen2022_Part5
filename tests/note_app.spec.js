d6d9a0d1418af117157f7e4b839e4767e8ea4f73
tonyguo0 <tonyguo998@gmail.com>
2025-12-30 12:15:10 +0800

    Organizes tests and adds user login test
    Wraps existing and new tests in a describe block to improve test organization and readability.
    
    Adds a new test case that verifies user login functionality by filling in credentials and confirming successful authentication.
    
    Updates the test environment script to use nodemon for automatic reload during development, improving the developer experience when working with tests.

--- tests/note_app.spec.js
+++ tests/note_app.spec.js
@@ -1,13 +1,26 @@
 const { test, expect } = require('@playwright/test');
+const { describe } = require('node:test');
 
-test('front page can be opened', async ({ page }) => {
-    await page.goto('http://localhost:5173');
+describe('Note app', () => {
+    test('front page can be opened', async ({ page }) => {
+        await page.goto('http://localhost:5173');
 
-    const locator = page.getByText('Notes');
-    await expect(locator).toBeVisible();
-    await expect(
-        page.getByText(
-            'Note app, Department of Computer Science, University of Helsinki 2025'
-        )
-    ).toBeVisible();
+        const locator = page.getByText('Notes');
+        await expect(locator).toBeVisible();
+        await expect(
+            page.getByText(
+                'Note app, Department of Computer Science, University of Helsinki 2025'
+            )
+        ).toBeVisible();
+    });
+
+    test(`user can log in`, async ({ page }) => {
+        await page.goto('http://localhost:5173');
+
+        await page.getByRole('button', { name: 'log in' }).click();
+        await page.getByLabel('username').fill('Tony');
+        await page.getByLabel('password').fill('gogotony');
+        await page.getByRole('button', { name: 'login' }).click();
+        await expect(page.getByText('Tony logged in')).toBeVisible();
+    });
 });
